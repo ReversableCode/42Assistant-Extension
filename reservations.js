da866a6b1fab615833b1c2892a00b4d8ba7ab923
Abdessamad El bahri <ael-bahr@e2r11p2.1337.ma>
2020-10-27 12:00:30 +0100

    Finished implementing 'Reserved Places' preview for reservation platform

--- reservations.js
+++ reservations.js
@@ -1,19 +1,34 @@
 // Listen for message from the background page that actually indicates that
 // the API request has been completed on the client side so we can run the code.
-
-let dataList = [];
-
-chrome.runtime.onMessage.addListener( function(req, sender, sendResponse) {
-	if (req.greeting == "reservations_update" && !dataList.length)
+chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
+	if (req.greeting == "reservations_update")
 	{
-		const xhr = new XMLHttpRequest();
-		xhr.open("GET", req.url, true);
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState === 4) {
-				dataList = JSON.parse(xhr.responseText)
-				const listOfSlots = document.getElementsByClassName('fc-content')
-			}
-		}
-		xhr.send();
+		window.postMessage({ type: "RESERVATION_42ASSISTANT", text: "START_SCRIPT" }, "*");
 	}
 });
+
+// Inject js code into page
+(function() {
+	const injectCode = `
+	window.addEventListener("message", function(event) {
+		// We only accept messages from ourselves
+		if (event.source != window)
+			return;
+
+		if (event.data.type && (event.data.type == "RESERVATION_42ASSISTANT")) {
+			const slotList = document.getElementById("app").__vue__.$children[1].events;
+			slotList.forEach(function(slot) {
+				const newTitle = [
+					slot.title,
+					'Reserved Places: ' + (slot.numberOfPlacesReserved)
+				].join('\\n');
+				slot.title = newTitle;
+			})
+		}
+	}, false);
+	`
+	const script = document.createElement('script');
+	script.textContent = injectCode;
+	(document.head||document.documentElement).appendChild(script);
+	script.remove();
+})();
